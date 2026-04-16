ead032de1e739730bcc2e1134dacae490b20f1fd
tapan0810 <arceus0810@gmail.com>
2026-04-16 23:10:13 +0530

    feat : Implemented Student Services

---
+++ DotNet_API_28/Services/IStudentService.cs
@@ -0,0 +1,15 @@
+﻿using DotNet_API_28.Entities.Models;
+
+namespace DotNet_API_28.Services
+{
+    public interface IStudentService
+    {
+        public List<Student> GetAllStudents();
+        public Student GetById(int id);
+        public Student Add(Student student);
+        public Student Update(Student student);
+        public bool Delete(int id);
+
+
+    }
+}
---
+++ DotNet_API_28/Services/StudentService.cs
@@ -0,0 +1,52 @@
+﻿using DotNet_API_28.Entities.Models;
+
+namespace DotNet_API_28.Services
+{
+    public class StudentService : IStudentService
+    {
+        private static List<Student> students = new()
+        {
+            new Student { Id =1,Name = "John Doe"},
+            new Student { Id =2,Name = "Jane Smith"}
+        };
+
+        public List<Student> GetAllStudents()
+        {
+            return students;
+        }
+
+        public Student GetStudentById(int id)
+        {
+            return students.FirstOrDefault(x => x.Id == id)!;
+        }
+
+        public Student Add(Student student)
+        {
+            student.Id = students.Max(x => x.Id) + 1;
+            students.Add(student);
+            return student;
+        }
+
+        public Student Update(Student student)
+        {
+            var existingStudent = students.FirstOrDefault(x => x.Id == student.Id);
+            if (existingStudent == null)
+            {
+                return null!;
+            }
+            existingStudent.Name = student.Name;
+            return existingStudent;
+        }
+
+        public bool Delete(int id)
+        {
+            var student = students.FirstOrDefault(x => x.Id == id);
+            if (student == null)
+            {
+                return false;
+            }
+            students.Remove(student);
+            return true;
+        }
+    }
+}
